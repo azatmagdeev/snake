49bee478b97af44973fde243d6be7f4fdb186154
azat <azatmagdeev@ya.ru>
2024-04-08 20:17:45 +0300

    игровое поле почти во весь экран

--- js/GameArea.js
+++ js/GameArea.js
@@ -1,13 +1,13 @@
 export class GameArea {
 
     cellPixels = 20
-    widthCells = 10
-    heightCells = 20
+    // widthCells = 10
+    // heightCells = 20
 
     constructor() {
         this.el = document.getElementById('gameArea');
-        // this.widthCells = Math.floor(window.innerWidth / this.cellPixels);
-        // this.heightCells = Math.floor((window.innerHeight / this.cellPixels)/2);
+        this.widthCells = Math.floor(window.innerWidth / this.cellPixels) - 2;
+        this.heightCells = Math.floor((window.innerHeight / this.cellPixels)) - 2;
         this.el.style.width = this.widthCells * this.cellPixels + 'px'
         this.el.style.height = this.heightCells * this.cellPixels + 'px'
         this.el.focus()
